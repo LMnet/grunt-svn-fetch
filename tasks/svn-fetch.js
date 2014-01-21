37772024b85e092f65bee5adfbeef7ee742937c7
Юрий Бадальянц <y.badalyants@pinpay.ru>
2014-01-21 14:52:51 +0700

    Full rebuild of plugin

--- tasks/svn-fetch.js
+++ tasks/svn-fetch.js
@@ -1,19 +1,14 @@
-/*
+/**
  * grunt-svn-fetch
  * https://github.com/jones/svn-fetch
  *
  * Copyright (c) 2013 Stephen Jones
  * Licensed under the MIT license.
  */
-
-'use strict';
-
 module.exports = function (grunt) {
+	"use strict";
 
-	// Please see the Grunt documentation for more information regarding task
-	// creation: http://gruntjs.com/creating-tasks
-
-	grunt.registerMultiTask('svn_fetch', 'Updates or checks out the desired files', function () {
+	grunt.registerMultiTask('svn-fetch', 'Updates or checks out the desired files', function () {
 		var exec = require('child_process').exec;
 		var options = this.options({
 			bin:         'svn',
@@ -23,44 +18,55 @@ module.exports = function (grunt) {
 		});
 		var done = this.async();
 
-		var processMap = function(map) {
-			var getNextMapping = function() {
-				if (paths.length > 0) {
-					processPath(paths.shift());
+		var Processor = function(){};
+		Processor.prototype = {
+			paths: null,
+			map: null,
+
+			processMap: function(map) {
+				if (map === undefined) {
+					grunt.log.error('\n\'map\' missing.');
+					return done(false);
+				}
+
+				this.map = map;
+				this.paths = Object.keys(this.map);
+				this.getNextMapping();
+			},
+
+			getNextMapping: function() {
+				if (this.paths.length > 0) {
+					this.processPath(this.paths.shift());
 				} else {
 					grunt.log.write('\n');
 					done(true);
 				}
-			};
+			},
 
-			var paths = Object.keys(map);
-			getNextMapping();
-
-			function processPath(path) {
+			processPath: function(path) {
+				var self = this;
 				var command = options.bin;
 				var fullPath = options.path + path;
 				if (grunt.file.exists(fullPath + '/.svn')) {
 					command = [ command, 'update', fullPath ].join(' ');
 				} else {
-					command = [ command, 'checkout', options.repository + map[path], fullPath ].join(' ');
+					command = [ command, 'checkout', options.repository + this.map[path], fullPath ].join(' ');
 				}
 				grunt.log.write('\nProcessing ' + fullPath);
 				exec(command, options.execOptions, function (error, stdout) {
 					grunt.log.write(stdout);
 					if (error !== null) {
 						grunt.log.error('\n#' + command + "\n" + error);
+						return done(false);
 					}
-					getNextMapping();
+					self.getNextMapping();
 				});
-			}
+			},
 		};
+		Processor.prototype.constructor = Processor;
 
+		//start processing
 		var map = this.data.map;
-		if (map !== undefined) {
-			processMap(map);
-		} else {
-			grunt.log.error('\n\'map\' missing.');
-			done(true);
-		}
+		(new Processor()).processMap(map);
 	});
 };
